b4ddbba60a2982f22280b79a113eae39b014beb3
Kenan Mammadli <kenan@doe.org>
2024-07-29 18:35:19 -0400

    added us2 sign in tests

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @CucumberOptions(
-            tags = "@REGRESSION",
+            tags = "@UserStory6",
             features = "src/test/resources",
             glue = "stepDefinitions",
             plugin = {
