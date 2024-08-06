cc4faab1640c2084827c6e7c04baed5670b1dca9
Kenan Mammadli <kenan@doe.org>
2024-08-06 10:50:05 -0400

    shared data

--- src/test/java/stepDefinitions/SharedData.java
+++ src/test/java/stepDefinitions/SharedData.java
@@ -4,13 +4,9 @@ import lombok.Data;
 
 @Data
 public class SharedData {
-
 // Yana your lines are from 11 to 31
 // Zina your lines are from 32 to 52
 // Kenan your lines are from 53 to 73
-
-
-
 
 
 }
