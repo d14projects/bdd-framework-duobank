8841ecadd9d6cdd53897d247ac4f7c5eaf7c09d8
Kenan Mammadli <kenan@doe.org>
2024-08-08 09:19:46 -0400

    db user story 1

--- src/test/java/stepDefinitions/db/SignupAndSigninStepDefs.java
+++ src/test/java/stepDefinitions/db/SignupAndSigninStepDefs.java
@@ -3,8 +3,10 @@ package stepDefinitions.db;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import utilities.DBUtils;
+import utilities.SeleniumUtils;
 
 import java.util.List;
+import java.util.Map;
 
 public class SignupAndSigninStepDefs {
 
@@ -19,6 +21,27 @@ public class SignupAndSigninStepDefs {
 
         System.out.println("Expected Column Names: "+ actualColumnNames);
         Assert.assertEquals(expectedColumnNames, actualColumnNames);
+    }
+
+
+    @Then("{string} table should not duplicate {string}.")
+    public void table_should_not_duplicate(String tableName, String key) {
+
+        String query = String.format("SELECT %s from %s",
+                key,
+                tableName);
+
+        System.out.println("Query: " + query);
+
+        List<Map<String, Object>> queryResult = DBUtils.getQueryResultListOfMaps(query);
+        System.out.println("Query Result: " + queryResult);
+
+        List<String> duplicateString = SeleniumUtils.findDuplicateString(queryResult, key);
+
+        System.out.println("Duplicate emails: " + duplicateString);
+
+        Assert.assertTrue(duplicateString.isEmpty());
+
 
     }
 }
