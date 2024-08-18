8a76326ec86f2af58d67043d27c561e55d2ddf69
Kenan Mammadli <kenan@doe.org>
2024-08-18 09:26:50 -0400

    API us3 first 6 tests

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
 
-            tags = "@REGRESSION",
+            tags = "@API",
 
             features = "src/test/resources",
             glue = "stepDefinitions",
